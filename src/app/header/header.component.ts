31f2b06187240e711f615a08c5e2006835e7a83b
Rajesh Narayanan <rajesh.narayanan@tallysolutions.com>
2018-09-17 19:22:29 +0530

    Add responsive styles

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalService } from '../global.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _global: GlobalService) { }
+
+  toggleSidenav() {
+    this._global.toggleSideNav();
+  }
 
   ngOnInit() {
   }
