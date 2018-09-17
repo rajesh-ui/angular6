c9a35e9599405df4e1d9ab88066974c4009e452b
Rajesh Narayanan <rajesh.narayanan@tallysolutions.com>
2018-09-17 18:25:11 +0530

    changes commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = VERSION.full;
+  users = [{name: 'cust1', email: 'cust1@email.com', tel: '12345666', created: new Date()},
+  {name: 'cust1', email: 'cust1@email.com', tel: '12345666', created: new Date()},
+  {name: 'cust1', email: 'cust1@email.com', tel: '12345666', created: new Date()},
+  {name: 'cust1', email: 'cust1@email.com', tel: '12345666', created: new Date()}];
+
+  addUser() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { GlobalService } from './global.service';
+
+import { AppRoutes } from './app.routes';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    HttpModule,
+    AppRoutes
   ],
-  providers: [],
+  providers: [GlobalService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
